98173c41db6f9a2bd4bb02d25dc6d8c8ad5aebc5
FloBraha <flory.fel@email.it>
2013-11-21 14:27:41 +0100

    Parata funzionante nel minigioco
    La parata fuziona col movimento del mouse

---
+++ FistsAreFlying/Assets/Scripts/Shield.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+
+
+	public float speed = 0.02f; 
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnMouseDrag()
+	{
+		screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+		
+		offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(0, Input.mousePosition.y, 0));
+
+			if (offset.y >0 && transform.position.y >-4) 
+				transform.Translate (Vector3.down * Time.deltaTime *speed);
+			if (offset.y < 0 && transform.position.y<4)
+
+				transform.Translate (Vector3.up * Time.deltaTime * speed);
+	}
+	
+}
+
