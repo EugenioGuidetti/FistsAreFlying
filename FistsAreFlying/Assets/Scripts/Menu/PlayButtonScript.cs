27ca9b1e42c80d6e2e5f4981b3752d56b799afd1
daven89 <daven8989@gmail.com>
2013-11-26 14:20:19 +0100

    Riorganizzazione scripts

---
+++ FistsAreFlying/Assets/Scripts/Menu/PlayButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButtonScript : MonoBehaviour {
+
+	GameObject firstChoice;
+	GameObject playChoice;
+	Vector3 translateCoordinate;
+	// Use this for initialization
+	void Start () {
+		playChoice= GameObject.Find("PlayChoice");
+		firstChoice= GameObject.Find("FirstChoice");
+		translateCoordinate=new Vector3(-20f,0f,0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		firstChoice.GetComponent<Transform>().Translate(translateCoordinate);
+		playChoice.GetComponent<Transform>().Translate(translateCoordinate);
+	}
+}
