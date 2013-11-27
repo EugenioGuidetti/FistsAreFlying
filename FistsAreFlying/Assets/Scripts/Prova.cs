f6768246d84ed202bb4b4396a7832870c975a93a
EugenioGuidetti <eugenioguidetti@yahoo.it>
2013-11-27 20:54:20 +0100

    Aggiornamento .gitignore

--- FistsAreFlying/Assets/Scripts/Prova.cs
+++ FistsAreFlying/Assets/Scripts/Prova.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Prova : MonoBehaviour {
 
+	float number;
+
 	// Use this for initialization
 	void Start () {
-	
+		number = 5f;
 	}
 	
 	// Update is called once per frame
