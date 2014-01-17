c8bbfa7b938f8d0a9351a3f79180d248fba20756
EugenioGuidetti <eugenioguidetti@yahoo.it>
2014-01-17 11:50:19 +0100

    Avevo dimenticato di controllare la forced move online.

--- FistsAreFlying/Assets/Scripts/Tablet/TPlayer.cs
+++ FistsAreFlying/Assets/Scripts/Tablet/TPlayer.cs
@@ -109,26 +109,32 @@ public class TPlayer : MonoBehaviour {
 	}
 
 	public void OnlineSelectMove (string move) {
-		selectedMove = move;
-		if (selectedMove.Equals("PR")) {
-			touchedMoves.Add(punchRight);
-		}
-		if (selectedMove.Equals("PL")) {
-			touchedMoves.Add(punchLeft);
-		}
-		if (selectedMove.Equals("KR")) {
-			touchedMoves.Add(kickRight);
-		}
-		if (selectedMove.Equals("KL")) {
-			touchedMoves.Add(kickLeft);
-		}
-		if (selectedMove.Equals("D")) {
-			touchedMoves.Add(defense);
-		}
-		if (selectedMove.Equals("EM")) {
+		if (!forcedMove) {
+			selectedMove = move;
+			if (selectedMove.Equals("PR")) {
+				touchedMoves.Add(punchRight);
+			}
+			if (selectedMove.Equals("PL")) {
+				touchedMoves.Add(punchLeft);
+			}
+			if (selectedMove.Equals("KR")) {
+				touchedMoves.Add(kickRight);
+			}
+			if (selectedMove.Equals("KL")) {
+				touchedMoves.Add(kickLeft);
+			}
+			if (selectedMove.Equals("D")) {
+				touchedMoves.Add(defense);
+			}
+			if (selectedMove.Equals("EM")) {
+				touchedMoves.Add(emptyMove);
+			}
+		} else {
+			selectedMove = "EM";
 			touchedMoves.Add(emptyMove);
 		}
 		SelectMove(touchedMoves[0]);
+		forcedMove = false;
 	}
 
 	public void ShowSelectedMove () {
