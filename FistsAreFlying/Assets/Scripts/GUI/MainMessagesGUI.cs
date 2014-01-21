c522d3f56f9827a511687c9118a50f0e7814886d
daven89 <daven8989@gmail.com>
2014-01-21 18:09:32 +0100

    continuazione inserimento grafica definitiva

---
+++ FistsAreFlying/Assets/Scripts/GUI/MainMessagesGUI.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMessagesGUI : MonoBehaviour {
+
+	public Sprite p1Turn;
+	public Sprite p2Turn;
+	public Sprite p1WinRound;
+	public Sprite p2WinRound;
+	public Sprite roundDraw;
+	public Sprite p1WinMatch;
+	public Sprite p2WinMatch;
+	public Sprite matchDraw;
+
+	// Use this for initialization
+	void Start () { }
+	
+	// Update is called once per frame
+	void Update () { }
+
+	public void SetSprite (string message) {
+		if (message.Equals("p1Turn")) {
+			this.GetComponent<SpriteRenderer>().sprite = p1Turn;
+			return;
+		}
+		if (message.Equals("p2Turn")) {
+			this.GetComponent<SpriteRenderer>().sprite = p2Turn;
+			return;
+		}
+		if (message.Equals("p1WinRound")) {
+			this.GetComponent<SpriteRenderer>().sprite = p1WinRound;
+			return;
+		}
+		if (message.Equals("p2WinRound")) {
+			this.GetComponent<SpriteRenderer>().sprite = p2WinRound;
+			return;
+		}
+		if (message.Equals("roundDrow")) {
+			this.GetComponent<SpriteRenderer>().sprite = roundDraw;
+			return;
+		}
+		if (message.Equals("p1WinMatch")) {
+			this.GetComponent<SpriteRenderer>().sprite = p1WinMatch;
+			return;
+		}
+		if (message.Equals("p2WinMatch")) {
+			this.GetComponent<SpriteRenderer>().sprite = p2WinMatch;
+			return;
+		}
+		if (message.Equals("matchDraw")) {
+			this.GetComponent<SpriteRenderer>().sprite = matchDraw;
+			return;
+		}
+		if (message.Equals("")) {
+			this.GetComponent<SpriteRenderer>().sprite = null;
+			return;
+		}
+	}
+}
